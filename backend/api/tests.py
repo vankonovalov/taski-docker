a976014184f14be34cd0269ec29abc74081416f4
Ivan Konovalov <vankonovalov@mail.ru>
2024-04-15 22:31:37 +0300

    Implement db for tests 5

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -2,7 +2,9 @@
 from http import HTTPStatus
 
 from api import models
+
 from django.test import Client, TestCase
+
 
 class TaskiAPITestCase(TestCase):
     def setUp(self):
@@ -19,4 +21,3 @@ class TaskiAPITestCase(TestCase):
         response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
         self.assertTrue(models.Task.objects.filter(title='Test').exists())
-
